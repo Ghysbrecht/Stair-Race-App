ef78c62f6e53c92e9f58a40c68c7c9e89d6dba7b
Michel Dequick <michel.dequick@gmail.com>
2017-11-06 11:27:46 +0100

    added mateialise

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,11 @@ ActiveRecord::Schema.define(version: 20171106094440) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
