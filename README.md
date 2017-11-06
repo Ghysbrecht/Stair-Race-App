7998931107bb8e37799b30082d71002d43ebf19a
Sil Boydens <sil.boydens@student.vives.be>
2017-11-06 10:47:51 +0100

    added participants

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ rails s
 
 # pages
 ```
-/Users/new
+/participants/new
 ```
 
 # pushing
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106085128) do
+ActiveRecord::Schema.define(version: 20171106094440) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "participants", force: :cascade do |t|
     t.string "name"
     t.string "uid"
     t.datetime "created_at", null: false
