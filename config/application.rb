23ecc5bc381687f3492694382e5e242df1e43e78
Sil Boydens <sil.boydens@student.vives.be>
2017-12-10 20:55:30 +0100

    allow thomas his android app to add participants

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module StairRaceApp
   class Application < Rails::Application
+    config.web_console.whitelisted_ips = '0.0.0.0/0'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
